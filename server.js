222d5fd30f3cb367e32f377fcf49a6c2e58a8835
Ben Cosmin <ben.iisr.2004@gmail.com>
2024-02-20 15:01:48 +0200

    clean code

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const { exec } = require('child_process');
 
 const app = express();
 const port = 3000;
+const fs = require('fs');
 
-// middleware body-parser
+// middleware to parse json data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/createfolder', (req, res) => {
+app.post('/createfolder', (req, res) => {
     // take folder name from query string
     const cmd = `mkdir ${req.query.foldername}`;
-   
-  
+     
     console.log(`Executing command: ${cmd}`)
   
     if (!cmd) {
@@ -30,54 +30,6 @@ app.get('/createfolder', (req, res) => {
     });
   });
 
-
-
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-
-
-
-
-
-// app.get('/admin', (req, res) => {
-//   const cmd = req.query.cmd;
-
-//   console.log(`Executing command: ${cmd}`)
-
-//   if (!cmd) {
-//     return res.status(400).send('No command provided');
-//   }
-
-//   exec(cmd, (error, stdout, stderr) => {
-//     if (error) {
-//       console.error(`Error executing command: ${error.message}`);
-//       return res.status(500).send('Error executing command');
-//     }
-
-//     console.log(`Command output: ${stdout}`);
-//     res.send(stdout);
-//   });
-// });
-
-// app.get('/showdata', (req, res) => {
-//   const cmd = req.query.cmd;
-
-//   console.log(`Executing command: ${cmd}`)
-
-//   if (!cmd) {
-//     return res.status(400).send('No command provided');
-//   }
-
-//   exec(cmd, (error, stdout, stderr) => {
-//     if (error) {
-//       console.error(`Error executing command: ${error.message}`);
-//       return res.status(500).send('Error executing command');
-//     }
-
-//     console.log(`Command output: ${stdout}`);
-//     res.send(stdout);
-//   });
-// });
